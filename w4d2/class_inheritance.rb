8304f7a4ee5df52ae070dcd52531b8c1777dc5fd
jhnegbrt <jhnegbrt@gmail.com>
2021-02-08 09:08:52 -0800

    starts class inheritance

--- w4d2/class_inheritance.rb
+++ w4d2/class_inheritance.rb
@@ -1,1 +1,34 @@
-xx
+class Employee
+
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+
+    end
+
+    def bonus(multiplier)
+
+        ##first multiplier
+        if @boss.nil?
+            bonus = multiplier * employees.sum
+        else
+            bonus = @salary * multiplier
+        end
+
+        #second multiplier
+
+    end
+
+end
+
+class Manager < Employee
+
+    def initialize(*subordinates)
+        @subs = []
+        subordinates.each {|sub| @sub << sub}
+    end
+
+end
