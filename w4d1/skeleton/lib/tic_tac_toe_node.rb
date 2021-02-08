c41d9bf9ea682e7f20636b71e58bed36e3eea975
jhnegbrt <jhnegbrt@gmail.com>
2021-02-07 19:28:04 -0800

    starts tic tac toe

--- w4d1/skeleton/lib/tic_tac_toe_node.rb
+++ w4d1/skeleton/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_mov_pos = prev_mov_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +16,19 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+
+    array = []
+    (0..2).each do |row|
+      (0..2).each do |col|
+        if @board[row][col].empty?
+          array << @board[row][col]
+        end
+      end
+    end
+    
+    
+
+    node = TicTacToeNode.new
+
   end
 end
