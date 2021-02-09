7a480a2bc17d7d73c46e187f2a68874d0c0d6e32
Justin Cheasty <cheasty@gmail.com>
2021-02-09 14:24:54 -0800

    did bonus method and manager class

--- w4d2/class_inheritance.rb
+++ w4d2/class_inheritance.rb
@@ -1,34 +1,35 @@
 class Employee
+  
+  attr_reader :salary
+  
+  def initialize(name, title, salary, boss)
+      @name = name
+      @title = title
+      @salary = salary
+      @boss = boss
+  end
 
-
-    def initialize(name, title, salary, boss)
-        @name = name
-        @title = title
-        @salary = salary
-        @boss = boss
-
-    end
-
-    def bonus(multiplier)
-
-        ##first multiplier
-        if @boss.nil?
-            bonus = multiplier * employees.sum
-        else
-            bonus = @salary * multiplier
-        end
-
-        #second multiplier
-
-    end
+  def bonus(multiplier)
+      if self.instance_of?(Manager)
+          sum = 0
+          self.subs.each { |sub| sum += sub.salary }
+          bonus = multiplier * sum
+      else
+          bonus = @salary * multiplier
+      end
+  end
 
 end
 
 class Manager < Employee
 
-    def initialize(*subordinates)
-        @subs = []
-        subordinates.each {|sub| @sub << sub}
-    end
+  attr_reader :subs
+
+  def initialize(name, title, salary, boss, subordinates)
+    super(name, title, salary, boss)
+    @subs = []
+    subordinates.each {|sub| @sub << sub}
+  end
 
 end
+
