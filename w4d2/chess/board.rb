11712c6be0c7916c204ee5224933457dc089b1ae
Justin Cheasty <cheasty@gmail.com>
2021-02-09 17:29:56 -0800

    populate board positions pt 1

--- w4d2/chess/board.rb
+++ w4d2/chess/board.rb
@@ -7,12 +7,27 @@ class Board
   #populate_board
 
   def populate_board
-    @rows[0].each_index do |idx|
-    end
+    self[0, 0] = SlideablePiece.new(args) #Rook
+    self[0, 1] = SteppingPiece.new(args)  #Knight
+    self[0, 2] = SlideablePiece.new(args) #Bishop
+    self[0, 3] = SlideablePiece.new(args) #Queen
+    self[0, 4] = SteppingPiece.new(args)  #King
+    self[0, 5] = SlideablePiece.new(args) #Bishop
+    self[0, 6] = SteppingPiece.new(args)  #Knight
+    self[0, 7] = SlideablePiece.new(args) #Rook
   
     @rows[2..5].each do |row|
       row.map! {|pos| NullPiece.instance }
     end
+
+    self[7, 0] = SlideablePiece.new(args) #Rook
+    self[7, 1] = SteppingPiece.new(args)  #Knight
+    self[7, 2] = SlideablePiece.new(args) #Bishop
+    self[7, 3] = SteppingPiece.new(args)  #King
+    self[7, 4] = SlideablePiece.new(args) #Queen
+    self[7, 5] = SlideablePiece.new(args) #Bishop
+    self[7, 6] = SteppingPiece.new(args)  #Knight
+    self[7, 7] = SlideablePiece.new(args) #Rook
   end
 
   # (3..6) rows to initialize nullpiece
