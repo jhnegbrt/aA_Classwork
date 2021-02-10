15c868527999ea91bdaaf931bf4f17c739c36536
Justin Cheasty <cheasty@gmail.com>
2021-02-09 16:58:24 -0800

    Organized classes

--- w4d2/chess/board.rb
+++ w4d2/chess/board.rb
@@ -4,4 +4,22 @@ class Board
     @rows = Array.new(8) {Array.new(8)}
   end
 
+  #populate_board
+
+  # (3..6) rows to initialize nullpiece
+  
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+  
+  def []=(pos, val)
+    row, col = pos
+    @rows[row][col] = val
+  end
+
+  def move_piece(start_pos, end_pos)
+
+  end
+
 end
--- w4d2/chess/piece.rb
+++ w4d2/chess/piece.rb
@@ -1,18 +1,37 @@
+require "Singleton"
+
 class Piece
 
   
 
   def initialize(symbol, pos)
     @color = symbol
-    @board = Board.
+    @board = Board.something #halp
     @pos = pos
   end
 
 end
 
 class NullPiece < Piece
+  include Singleton
 
-  def initialize
-  end
+  def initialize; end
 
+end
+
+class SlideablePiece
+end
+
+module Slideable
+  
+end
+
+
+class SteppingPiece
+end
+
+module Stepable
+end
+
+class Pawn
 end
