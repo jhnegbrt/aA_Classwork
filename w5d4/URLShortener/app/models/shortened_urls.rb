94dc9f2e4802bc8a1b92de651df7b5cea621db07
jhnegbrt <jhnegbrt@gmail.com>
2021-02-18 16:53:05 -0800

    continues shortenedurl project

---
+++ w5d4/URLShortener/app/models/shortened_urls.rb
@@ -0,0 +1,7 @@
+class ShortenedUrls < ApplicationRecord
+
+  validates :short_url, presence: true, uniqueness: true
+  validates :long_url, presence: true
+  validates :user_id, presence: true
+
+end
--- w5d4/URLShortener/db/migrate/20210219001741_shortened_urls.rb
+++ w5d4/URLShortener/db/migrate/20210219001741_shortened_urls.rb
@@ -10,10 +10,10 @@ class ShortenedUrls < ActiveRecord::Migration[5.2]
 
     end
 
-  end
+    add_index :shortened_urls, :short_url, unique: true
+    add_index :shortened_urls, :long_url
+    add_index :shortened_urls, :user_id
 
-  add_index :shortened_urls, :short_url, unique: true
-  add_index :shortened_urls, :long_url
-  add_index :shortened_urls, :user_id
+  end
 
 end
