4d890bb3f0ae8e842b78c0ca633ebdf17ede43e0
Ali Ibsin <ibsin.a1996@gmail.com>
2021-02-18 17:24:19 -0800

    continue URLShortener

--- w5d4/URLShortener/app/models/shortened_urls.rb
+++ w5d4/URLShortener/app/models/shortened_urls.rb
@@ -1,7 +1,19 @@
+require 'securerandom'
+
 class ShortenedUrls < ApplicationRecord
 
   validates :short_url, presence: true, uniqueness: true
   validates :long_url, presence: true
   validates :user_id, presence: true
+
+  def self.random_code
+    
+    code = SecureRandom.base64
+    until !ShortenedUrls.exists?(code)
+      code = SecureRandom.base64
+    end
+    return code
+    
+  end
 
 end
