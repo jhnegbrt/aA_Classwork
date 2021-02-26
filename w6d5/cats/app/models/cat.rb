6834b61cc06421ab7caa847d1d37acc739d047d7
jhnegbrt <jhnegbrt@gmail.com>
2021-02-26 14:50:44 -0800

    adds cat age

---
+++ w6d5/cats/app/models/cat.rb
@@ -0,0 +1,24 @@
+require "date"
+
+class Cat < ApplicationRecord
+
+  attr_accessor :birth_date
+
+  # new_cat = Cat.find_by(:birth_date)
+
+  # new_cat = Cat.new("2010/01/01")
+
+  def age
+
+    # date = Date.new
+    current = Date.today.year
+    birth_date = self.birth_date.to_s
+    year = birth_date.split("-")
+    
+  
+    age = (current - year[0].to_i).to_s
+
+  end
+
+  
+end
