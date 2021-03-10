502d7d584cf228561d5929236f3bb17cabe61efe
jhnegbrt <jhnegbrt@gmail.com>
2021-03-10 10:59:41 -0800

    adds inject

--- w8d3/JavascriptExercises/phase_2_enumerable.js
+++ w8d3/JavascriptExercises/phase_2_enumerable.js
@@ -1,14 +1,41 @@
-function callback(num) {
+function square(num) {
   return num * num;
 }
-Array.prototype.my_each = function(callback) {
+
+
+Array.prototype.myEach = function(eachCallback) {
   for(let i = 0; i < this.length; i++){
-    callback(this[i])
+    eachCallback(this[i])
   }
 }
 
-Array.prototype.my_map = function (callback) {
+
+Array.prototype.myMap = function (mapCallback) {
   let array = []
-  this.my_each(callback)
+  this.myEach( ele => {
+    array.push(mapCallback(ele))
+  })
+  return array
   
 }
+
+Array.prototype.myReduce = function(callback, initialValue = 0){
+  let arr = this
+  if (!initialValue){
+    initialValue = this[0]
+    console.log(this.length)
+    console.log(arr.length)
+    arr = arr.slice(1)
+  }
+
+  arr.myEach( ele => {
+    initialValue += callback(ele)
+  })
+
+  return initialValue
+
+}
+
+
+
+
