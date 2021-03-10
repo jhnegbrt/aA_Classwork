1ddec72b6bd8e24f365355c3d3a21dd01324592b
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-03-10 14:47:38 -0800

    complete 2nd question

--- w8d3/JavascriptExercises/phase_1_arrays.js
+++ w8d3/JavascriptExercises/phase_1_arrays.js
@@ -11,8 +11,18 @@ Array.prototype.uniq = function() {
   return array;
 }
 
-Array.prototype.two_sum = () => {
+Array.prototype.two_sum = function () {
+  let array = []
+  for(let i = 0; i < this.length - 1; i++){
 
-  
+    for(let j = 1; j < this.length; j++){
 
+      if (this[j] + this[i] === 0 && j > i){
+        let sub_arr = [i, j]
+        array.push(sub_arr)
+      }
+    }
+  }
+  return array;
 }
+
