5f1a301d2b06003c6856542bdb731108772ee214
jhnegbrt <jhnegbrt@gmail.com>
2021-04-17 17:41:22 -0700

    begins mern twitter user auth

--- MERNstack/mernTwitter/app.js
+++ MERNstack/mernTwitter/app.js
@@ -3,12 +3,45 @@ const express = require("express")
 const app = express()
 const db = require('./config/keys').mongoURI
 const port = process.env.PORT || 5000
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
+const User = require('./models/User');
+
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send('Hello World'))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "1",
+    email: "1",
+    password: "1"
+  })
+  user.save()
+  res.send('Hello World')
+
+})
+
+app.use("/api/users", users)
+app.use("/api/users", tweets)
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
+});
---
+++ MERNstack/mernTwitter/routes/api/users.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const keys = ('../../config/keys')
+
+router.get("/test", (req, res) =>{
+  res.json({
+    msg: "This is the user route"
+  })
+})
+
+router.post('/register', (req, res)=>{
+
+  // const { errors, isValid } = validateRegisterInput(req.body);
+
+  // if (!isValid){
+  //   return res.status(400).json(errors)
+  // }
+  User.findOne({ handle: req.body.handle })
+    .then(user => {
+      if (user) {
+        errors.handle = "User already exists"
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                const payload = { id: user.id, handle: user.handle }
+
+                jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) =>{
+                  debugger
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                })
+              })
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+})
+
+router.post('/login', (req, res) => {
+  // const { errors, isValid } = validateLoginInput(req.body)
+  const handle = req.body.handle;
+  const password = req.body.password;
+
+  // if (!isValid){
+  //   return res.status(400).json(errors)
+  // }
+
+  User.findOne({handle})
+    .then(user => {
+      if (!user) {
+        errors.handle = "User not found"
+        return res.status(400).json(errors);
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, handle: user.handle}
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) =>{
+                res.json({
+                  succcess: true,
+                  token: 'Bearer ' + token
+                })
+              }
+            )
+          } else {
+            errors.password = "Incorrect password";
+            return res.status(400).json(errors)
+          }
+        })
+    })
+})
+
+
+module.exports = router;
